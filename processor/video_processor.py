a9554eb5d84099909b08b7f225f293a633514ff1
Lanzhijiang <lanzhijiang@foxmail.com>
2023-04-20 09:23:49 +0800

    video_processor: video/audio to text

---
+++ processor/video_processor.py
@@ -0,0 +1,35 @@
+# env: python3.10 pytorch+cu1.7 
+# author: Lanzhijiang
+# date: 2023/04/19
+# description: 
+#   处理视频信息，并作总结
+#   1. 通过whisper对视频音频转文字，并且分句
+#   2. 语意理解以分类视频
+
+import json
+import whisper
+
+settings = json.load(open("./data/settings.json", "r"), encoding="utf-8")
+
+class VideoProcessor:
+
+    BASE_OUPUT_PATH = "./"
+
+    @classmethod
+    def audio_to_text(cls, audio_fp, model_id="small"):
+
+        """
+        语音转文字
+        """
+        model = whisper.load_model(model_id)
+        result = model.transcribe(audio_fp)["text"]
+        return result
+        
+    @classmethod
+    def video_to_text(cls, video_fp):
+        
+        """
+        视频转文字
+            不同于语音，采用OCR技术
+        """
+
